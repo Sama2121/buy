24b6887a56f91d6d07838a6ea37a1c0b7ca1991c
Sama2121 <34972242+Sama2121@users.noreply.github.com>
2017-12-31 09:17:37 +0900

    Add files via upload

---
+++ ikeura1.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(void){
+	
+	int i,n;
+	int m=1;
+	int a=10;
+	int b=10;
+
+	for(i=0;i<a;i++){
+
+		for(n=1;n<b;n++){
+			printf(" ");
+		}
+		printf("*");
+		for(n=1;n<m;n++){
+			printf("**");
+		}
+		m++;
+		b--;
+		printf("\n");
+	}
+}
